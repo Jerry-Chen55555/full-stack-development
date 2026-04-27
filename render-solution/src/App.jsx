4e1ee5e357644d42147ce1b3369193dd31304136
Jerry-Chen55555 <jerryc1825@gmail.com>
2026-04-27 10:02:29 -0400

    Incremental

--- render-solution/src/App.jsx
+++ render-solution/src/App.jsx
@@ -1,16 +1,26 @@
-const App = () => {
-  // jscode
-  let counter = 0
+import { useState } from "react"
 
-  const increaseCounter = () => {
-    counter++
+const App = () => {
+  const [counter, setCounter] = useState(1)
+
+  console.log("rendered")
+  const plusOne = () => {
+    setCounter(counter + 1)
+  }
+  const timesTwo = () => {
+    setCounter(counter * 2)
+  }
+  const secondPower = () => {
+    setCounter(counter ** 2)
   }
 
   return (
     <div>
-      <h2>Render solution</h2>
-      {/* <p>Counter: {counter}</p>
-      <button onClick={increaseCounter}>Increment</button> */}
+      <h2>Render solution - using React Hooks</h2>
+      <p>Counter: {counter}</p>
+      <button onClick={plusOne}>+1</button>
+      <button onClick={timesTwo}>*2</button>
+      <button onClick={secondPower}>^2</button>
     </div>
   )
 }
