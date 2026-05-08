1006a88ad8195c5f51bdcd45ddd577258e7999f6
Jerry-Chen55555 <jerryc1825@gmail.com>
2026-05-08 15:12:25 -0400

    JS Jokes Lab

--- Chapter 2/seinfield/src/components/Season.jsx
+++ Chapter 2/seinfield/src/components/Season.jsx
@@ -1,8 +1,10 @@
 const Season = ({season}) => {
+    const totalViews = season.episodes.reduce((acc, episode) => acc + episode.views, 0);
     return (
         <>
             <h2>{season.title}</h2>
             {season.episodes.map((episode)=>(<p key={episode.id}>{episode.title} - Views: {episode.views}</p>))}
+            <p>Total Views: {totalViews}</p>
         </>
     )
 }
