71a95de83f667ccf64e838120b8f2853a827a5cc
Jerry-Chen55555 <jerryc1825@gmail.com>
2026-05-11 15:20:20 -0400

    Class 10

--- Chapter 2/movies/src/App.jsx
+++ Chapter 2/movies/src/App.jsx
@@ -1,17 +1,41 @@
+import { useState } from "react"
 import Movie from "./components/Movie"
 
-const App = ({movies}) => {
+const App = ({initialMovies}) => {
+  const [movies, setMovies] = useState(initialMovies)
+  const [movieName, setName] = useState("")
+
+  const addMovie = (e) => {
+    e.preventDefault()
+    console.log("Movie: ", movieName)
+    setName("")
+    setMovies([...movies, {id: movies.length + 1, title: movieName, watchList: true}])
+  }
+
   return (
     <div>
       <h2>Movies</h2>
       <ul>
         {movies.map((movie)=>(<Movie key={movie.id} movie={movie}/>))}
       </ul>
+      <form onSubmit={addMovie}>
+        {/* uncontrolled needs a name */}
+        {/* you can use a state for controlled input */}
+        <input onChange={(e)=>setName(e.target.value)} value={movieName} />
+        <button type="submit">Add Movie</button>
+      </form>
     </div>
   )
 }
 
 export default App
+
+// uncontrolled input field
+// const addMovie = (e) => {
+//   e.preventDefault()
+//   console.log("Movie: ", e.target.movieName.value)
+//   e.target.movieName.value = ""
+// }
 
 {/* {movies.map((m)=>{
   return <li>{m.title}</li>
--- Chapter 2/movies/src/main.jsx
+++ Chapter 2/movies/src/main.jsx
@@ -12,6 +12,4 @@ const movies = [
   { id: 8, title: "The Social Network", watchlist: false }
 ];
 
-createRoot(document.getElementById('root')).render(
-  <App movies={movies}/>
-)
+createRoot(document.getElementById('root')).render(<App initialMovies={movies}/>)
